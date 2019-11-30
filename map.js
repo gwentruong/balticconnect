c437f3bc713a408870b3b16176ff8c28d635c26d
Uyen Truong <37957397+gwentruong@users.noreply.github.com>
2019-12-01 00:07:22 +0200

    Vector visualization (#3)
    * Change zoom level when toogling
    
    * Changed logo and add credits
    
    * Delete and ignore incompatilble plugins and solutions
    
    * Add nutrients files
    
    * Add toogle buttons for P and N

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 var map;
-// var layerControl;
+var PLayer;
+var NLayer;
 
 function onEachFeature(feature, layer) {
     if (feature.properties && feature.properties.Name) {
@@ -14,16 +15,22 @@ function onEachFeature(feature, layer) {
     }
 }
 
+function handleFeature(feature, layer) {
+    layer.bindPopup('<strong>'+ feature.properties.name +
+                    '</strong><br/>'+ feature.properties.country
+                    +'<br/>' + feature.properties.helcomId)
+}
+
 function main() {
     var oceanMap = L.tileLayer.provider('CartoDB.Positron');
     var imageryMap = L.tileLayer.provider('Esri.WorldImagery');
 
     // Load basemap with 2 styles
     var map = L.map('map', {
-        center: [60.359708, 22.021643],
-        zoom: 8,
-        minZoom: 6,
-        maxZoom: 12,
+        center: [60.295895, 21.903840],
+        zoom: 9,
+        minZoom: 8,
+        maxZoom: 11,
         layers: [oceanMap]
     });
 
@@ -44,7 +51,7 @@ function main() {
     var bathymetric = source.getLayer('BATHYMETRIC');
 
     // Load geojson of activities points
-    $.getJSON("./layers/Activities_Database_TurkuExample.geojson",function(data){
+    $.getJSON("./layers/Activities_Database_TurkuExample.geojson", function(data){
         var markerIcon = L.icon({
         iconUrl: './img/marker.svg',
         iconSize: [50,40]
@@ -56,45 +63,134 @@ function main() {
         onEachFeature: onEachFeature
       });
 
+      // Toogle button to enable/disable activities
       var toggle = L.easyButton({
           states: [{
-            stateName: 'add-markers',
-            icon: '&check;',
-            title: 'Show activities',
-            onClick: function(control) {
-              map.addLayer(activityTurku);
-              control.state('remove-markers');
-            }
+              stateName: 'add-markers',
+              icon: '&check;',
+              title: 'Show activities',
+              onClick: function(control) {
+                  map.setView([60.448861, 22.265566], 11);
+                  map.addLayer(activityTurku);
+                  control.state('remove-markers');
+              }
           }, {
-            icon: '&cross;',
-            stateName: 'remove-markers',
-            onClick: function(control) {
-              map.removeLayer(activityTurku);
-              control.state('add-markers');
-            },
-            title: 'Hide activities'
+              icon: '&cross;',
+              stateName: 'remove-markers',
+              onClick: function(control) {
+                  map.removeLayer(activityTurku);
+                  map.setView([60.359708, 22.021643], 9);
+                  control.state('add-markers');
+              },
+              title: 'Hide activities'
           }]
       });
       toggle.addTo(map);
+    });
+
+    // Add nutrient vector layers
+    map.createPane("Total phosphorus");
+    map.createPane("Total nitrogen");
+
+    $.getJSON('./layers/nutrient_2018.geojson', function(vectorData) {
+        PLayer = L.geoJson(vectorData, {
+            style: function(feature) {
+                var fillColor;
+                var status = feature.properties.phosphorus;
+                if (status == 'Fail') fillColor = "#e6ac25";
+                else if (status == 'Achieve') fillColor = "#32bf3e";
+                else fillColor = "#FFFFFFFF";
+                return { color: "#FFFFFFFF", weight: 0, fillColor: fillColor, fillOpacity: .4 };
+            },
+            onEachFeature: handleFeature,
+            pane : "Total phosphorus"
+        });
+
+        var Pbutton = L.easyButton({
+            states: [{
+                stateName: 'add-markers',
+                icon: '&Rho;',
+                title: 'Show total phosphorus',
+                onClick: function(control) {
+                    map.addLayer(PLayer);
+                    control.state('remove-markers');
+                }
+            }, {
+                icon: '&cross;',
+                stateName: 'remove-markers',
+                onClick: function(control) {
+                    map.removeLayer(PLayer);
+                    control.state('add-markers');
+                },
+                title: 'Hide total phosphorus'
+            }]
+        });
+        Pbutton.addTo(map);
+
+        NLayer = L.geoJson(vectorData, {
+            style: function(feature) {
+                var fillColor;
+                var status = feature.properties.nitrogen;
+                if (status == 'Fail') fillColor = "#e6ac25";
+                else if (status == 'Achieve') fillColor = "#32bf3e";
+                else fillColor = "#FFFFFFFF";
+                return { color: "#FFFFFFFF", weight: 0, fillColor: fillColor, fillOpacity: .4 };
+            },
+            onEachFeature: handleFeature,
+            pane : "Total nitrogen"
+        });
+
+        var Nbutton = L.easyButton({
+            states: [{
+                stateName: 'add-markers',
+                icon: '&Nu;',
+                title: 'Show total nitrogen',
+                onClick: function(control) {
+                    map.addLayer(NLayer);
+                    control.state('remove-markers');
+                }
+            }, {
+                icon: '&cross;',
+                stateName: 'remove-markers',
+                onClick: function(control) {
+                    map.removeLayer(NLayer);
+                    control.state('add-markers');
+                },
+                title: 'Hide total nitrogen'
+            }]
+        });
+        Nbutton.addTo(map);
     });
 
     // Combine overlay layers
     var groupOverLays = {
         "Land development": {
             "Agriculture": agriculture,
-            "Vegetation index (NDVI)": ndvi
+            "Vegetation index (NDVI)": ndvi,
         },
         "Water quality": {
             "Ocean depth (Bathymetric)": bathymetric
         }
     };
 
+    var options = {
+      // Make the "Land development" group exclusive (use radio inputs)
+      exclusiveGroups: ["Land development"]
+    };
+
     // Group basemap and overlay layers
-    L.control.groupedLayers(baseLayers, groupOverLays).addTo(map);
+    var layerControl = L.control.groupedLayers(baseLayers, groupOverLays, options);
+    map.addControl(layerControl);
 
     // Add map scale
     L.control.scale({
         position: 'bottomleft'
+    }).addTo(map);
+
+    var credctrl = L.controlCredits({
+        image: "./img/logo.png",
+        link: "http://balticsatapps.eu/weseachallenge/",
+        text: "Baltic Connect, participant of <u>WeSeachallenge</u>"
     }).addTo(map);
 }
 
