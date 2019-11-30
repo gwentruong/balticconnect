668f3d6ab6dc75c94630bce440dd8c5d83ad8100
Uyen Truong <haiuyentruong@gmail.com>
2019-11-30 15:57:22 +0200

    Add basic WMS layers

--- map.js
+++ map.js
@@ -1,35 +1,18 @@
 var map;
 var layerControl;
 
-function initMap() {
+function main() {
     var oceanMap = L.tileLayer.provider('Esri.OceanBasemap');
     var imageryMap = L.tileLayer.provider('Esri.WorldImagery');
-    map = L.map('map', {
+
+    var map = L.map('map', {
         center: [60.359708, 22.021643],
         zoom: 8,
         minZoom: 6,
         maxZoom: 9,
         layers: [oceanMap]
     });
-    layerControl = L.control.layers({
-        'ERSI Ocean Map': oceanMap,
-        'ERSI Imagery': imageryMap
-    }, {}).addTo(map);
-    L.hash(map);
-}
 
-function drawCanvasLayer(url, name, options) {
-    var name = name || "canvas";
-
-    corslite(url, function (err, resp) {
-        var json = JSON.parse(resp.response);
-        var canvasLayer = L.tileLayer.canvas.geoJson(json, options).addTo(map);
-        layerControl.addOverlay(canvasLayer, name);
-    }, true);
-}
-
-function main() {
-    initMap();
     var options = {
         maxZoom: 16,
         tolerance: 3,
@@ -42,9 +25,36 @@ function main() {
             fillOpacity:0.5
         }
     };
-    // drawCanvasLayer('http://handygeospatial.github.io/geojsonvt-ksj-site/kanagawa_yoto.geojson', 'polygon', options)
-    // drawCanvasLayer('./greenway.json', 'polyline', options)
-    // drawCanvasLayer('./busstop.json', 'point', options)
+    var baseLayers = {
+        'ERSI Ocean Map': oceanMap,
+        'ERSI Imagery': imageryMap
+    };
+
+    var option_wms = {
+        transparent: true,
+        format: 'image/png',
+        opacity: 0.5
+    };
+    var source = L.WMS.source('https://services.sentinel-hub.com/ogc/wms/c28647c2-61b0-4e76-909e-bc6e30c634a1?', option_wms);
+    var agriculture = source.getLayer('AGRICULTURE');
+    var ndvi = source.getLayer('NDVI');
+    var bathymetric = source.getLayer('BATHYMETRIC');
+
+    var groupOverLays = {
+        "Land development": {
+            "Agriculture": agriculture,
+            "Vegetation index (NDVI)": ndvi
+        },
+        "Water quality": {
+            "Ocean depth (Bathymetric)": bathymetric
+        }
+    };
+
+    L.control.groupedLayers(baseLayers, groupOverLays).addTo(map);
+
+    L.control.scale({
+        position: 'bottomleft'
+    }).addTo(map);
 }
 
 main();
